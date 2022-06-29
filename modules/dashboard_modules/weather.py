3343c5ff5381cd2a8c48afc6e12ea0197f5e6ad2
erika <no>
2022-06-29 18:19:00 +0800

    I GOTTA FINISH IT TOMORROW

--- modules/dashboard_modules/weather.py
+++ modules/dashboard_modules/weather.py
@@ -5,9 +5,17 @@ from requests.exceptions import Timeout
 from requests.models import HTTPError
 
 from modules.dashboard_modules.util import Widget
+from modules.dashboard_modules.weather_constants import WeatherCodeDatabase
+
 import time
+import cairo
+import common.context
+from common.context import _DPI
+from gi.repository import Pango
 
 LINK = "https://wttr.in/?format=j1"
+
+LARGE_HEADER_FONT = Pango.FontDescription('Cantarell 30')
 
 class Temprature:
     celsius = 0
@@ -32,23 +40,51 @@ class WeatherWidget(Widget):
 
             self.weather_data = WeatherData()
             self.weather_data.weather_code = int(current_condition['weatherCode'])
-            self.weather_data.temprature = new_temp 
+            self.weather_data.info = WeatherCodeDatabase.get(self.weather_data.weather_code)
+            self.weather_data.temprature = new_temp
+            self.loaded = True
         except (ConnectionError, HTTPError, Timeout, KeyError) as e:
             print(f'failed to get weather data: {e}')
 
     def __init__(self):
-        super().__init__(y=305, width=300, height=275)
+        super().__init__(y=305, width=350, height=275)
         
         self.weather_data = None
         self.loaded = False
         self._lt = 0
+        self._r = 0
 
         t = Thread(target=self.load_weather_data)
         t.daemon = True
         t.start()
+    
+    def update_widget(self, dt):
+        self._r += 10 * dt
+        self._lt = min(self._lt + (dt * self.loaded * 5), 1)
 
     def draw_widget(self, ctx, layout):
         ctx.set_source_rgba(1, 1, 1, self.alpha)
+       
+        rev_lt = 1 - self._lt
 
+        if self.weather_data is not None and self._lt > 0:
+            ctx.save()
+            layout.set_font_description(LARGE_HEADER_FONT)
+            ctx.set_source_rgba(1, 1, 1, self.alpha * self._lt)
+            ctx.save()
+            ctx.scale(3, 3)
+            common.context.text(ctx, layout, self.weather_data.info.icon)
+            ctx.restore()
+            ctx.restore()
 
-
+        if self._lt < 1:
+            ctx.save()
+            ctx.set_source_rgba(0.2, 0.2, 0.2, (self.alpha * 0.6) * rev_lt) 
+            common.context.rounded_rectangle(ctx, (self.width * 0.5) - 50 - self.padding, (self.height * 0.5) - 50 - self.padding, 100, 100, 10)
+            ctx.fill()
+            ctx.set_line_width(10)
+            ctx.set_line_cap(cairo.LineCap.ROUND)
+            ctx.set_source_rgba(1, 1, 1, self.alpha * rev_lt)
+            ctx.arc(self.width * 0.5 - self.padding, self.height * 0.5 - self.padding, 30, self._r - .25, self._r + (_DPI * 0.455 - 0.25))
+            ctx.stroke()
+            ctx.restore() 
---
+++ modules/dashboard_modules/weather_constants.py
@@ -0,0 +1,61 @@
+class DatabaseItem:
+    def __init__(self, name, icon):
+        self.name = name
+        self.icon = icon
+
+WeatherCodeDatabase = {
+    113: DatabaseItem('sunny', ''),
+    116: DatabaseItem('partly cloudy', ''),
+    119: DatabaseItem('cloudy', ''),
+    122: DatabaseItem('very cloudy', ''),
+    143: DatabaseItem('fog', ''),
+    176: DatabaseItem('light showers', ''),
+    179: DatabaseItem('light sleet', ''),
+    182: 179,
+    185: 179,
+    200: DatabaseItem('thunderstorm', ''),
+    227: DatabaseItem('light snow', ''),
+    230: DatabaseItem('heavy snow', ''),
+    248: 143,
+    260: 143,
+    263: 176,
+    266: DatabaseItem('light rain', ''),
+    281: 179,
+    284: 179,
+    293: 266,
+    296: 266,
+    299: DatabaseItem('heavy showers', ''),
+    302: DatabaseItem('heavy rain', ''),
+    305: 299,
+    308: 302,
+    311: 179,
+    314: 179,
+    317: 179,
+    320: 179,
+    323: 227,
+    326: 227,
+    329: 230,
+    332: 230,
+    335: 230,
+    338: 230,
+    350: 179,
+    353: 176,
+    356: 299,
+    359: 299,
+    362: 179,
+    365: 179,
+    368: 230,
+    371: 230,
+    374: 179,
+    377: 179,
+    386: 200,
+    389: 200,
+    392: DatabaseItem('thunder show', ''),
+    395: 230
+}
+
+for key, value in WeatherCodeDatabase.items():
+    if not isinstance(value, int):
+        continue
+
+    WeatherCodeDatabase[key] = WeatherCodeDatabase[value]
